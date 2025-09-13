8d2838af678959c9fad56cfab730374a68643721
palle24081993 <prashanthreddy0787@gmail.com>
2025-09-13 00:44:59 -0500

    Update Linked_List_Implementation.cpp

--- Linked_List_Implementation.cpp
+++ Linked_List_Implementation.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
 using namespace std;
+
 struct Node {
-    int row, column, value;  
-    Node* next;          
+    int row, column, value;
+    Node* next;
     Node(int r, int c, int v) : row(r), column(c), value(v), next(nullptr) {}
 };
 
 class SPMLinkedList {
 private:
-    Node* head;  
+    Node* head;
 
 public:
     SPMLinkedList() : head(nullptr) {}
 
-    void insert(int row, int column, int value) {
-        Node* newNode = new Node(row, column, value);  
-        if (head == nullptr) {
-            head = newNode;  
+    void insert(int r, int c, int v) {
+        Node* newNode = new Node(r, c, v);
+        if (!head) {
+            head = newNode;
         } else {
             Node* temp = head;
-            while (temp->next != nullptr) {  
-                temp = temp->next;
-            }
-            temp->next = newNode;  
+            while (temp->next) temp = temp->next;
+            temp->next = newNode;
         }
     }
 
@@ -38,11 +37,11 @@ public:
                  << ", Column: " << temp->column
                  << ", Value: " << temp->value
                  << ", Next Node Address: " << temp->next << endl;
-            temp = temp->next;  // Move to the next node
+            temp = temp->next;
         }
     }
 
-    ~SparseMatrixLinkedList() {
+    ~SPMLinkedList() {
         Node* temp;
         while (head != nullptr) {
             temp = head;
@@ -53,28 +52,28 @@ public:
 };
 
 int main() {
-    int rows, column, value;
+    int rows, cols, value;
 
     SPMLinkedList spm;
 
     cout << "Enter the number of rows: ";
     cin >> rows;
     cout << "Enter the number of columns: ";
-    cin >> column;
+    cin >> cols;
 
     cout << "Enter the elements of the matrix (0 for empty):\n";
-    
     for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < column; ++j) {
+        for (int j = 0; j < cols; ++j) {
             cout << "Element at Row " << i << ", Column " << j << ": ";
             cin >> value;
             if (value != 0) {
-                spm.insert(i, j, value);  
+                spm.insert(i, j, value);
             }
         }
     }
+
     cout << "\nSparse Matrix Linked List Representation:\n";
-    spm.display();  
+    spm.display();
 
     return 0;
 }
